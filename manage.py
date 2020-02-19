3c10cbb1a865f1ee8ebf8b26befb53e54d3eb4b8
monicaoyugi <monicaoyugi@gmail.com>
2020-02-19 07:40:04 +0300

    create app development

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ manage.add_command('server', Server)
 
 @manage.command
 def test():
-    '''Run the unit tests.'''
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
